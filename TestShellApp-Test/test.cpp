adafb9da095fb80e8949754f4070fd1dfb08524e
JJY <binaryfalse81@gmail.com>
2024-07-08 13:19:56 +0900

    TestShell Driver -> SSD Driver 로 이름 변경

--- TestShellApp-Test/test.cpp
+++ TestShellApp-Test/test.cpp
@@ -1,9 +1,33 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include "../Test_Shell_App/SsdDriver.h"
+#include "../Test_Shell_App/TestShell.cpp"
 
+using namespace std;
 using namespace testing;
 
-TEST(TestCaseName, TestName) {
-	EXPECT_EQ(1, 1);
-	EXPECT_TRUE(true);
+class MockSsdDriver : public SsdDriver
+{
+public:
+    //MOCK_METHOD(void, write, (int addr, int value), (override));
+    //MOCK_METHOD(int, read, (int addr), (override));
+};
+
+class TestShellFixture : public testing::Test
+{
+protected:
+    void SetUp() override
+    {
+    }
+public:
+};
+
+TEST_F(TestShellFixture, unmap_read)
+{
+    EXPECT_EQ(0, 0);
+}
+
+TEST_F(TestShellFixture, unmap_read_oor)
+{
+    EXPECT_EQ(0, 0);
 }
