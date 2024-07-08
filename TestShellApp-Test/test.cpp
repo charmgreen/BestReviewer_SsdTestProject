8885cda2748c1637af17d201fa17a6e10e877981
JJY <binaryfalse81@gmail.com>
2024-07-08 14:28:49 +0900

    read ut commit

--- TestShellApp-Test/test.cpp
+++ TestShellApp-Test/test.cpp
@@ -1,33 +1,42 @@
+// Copyright [2024] <JJY>
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 #include "../Test_Shell_App/SsdDriver.h"
-#include "../Test_Shell_App/TestShell.cpp"
+#include "../Test_Shell_App/TestShell.h"
 
 using namespace std;
 using namespace testing;
 
-class MockSsdDriver : public SsdDriver
-{
+class MockSsdDriver : public SsdDriver {
 public:
-    //MOCK_METHOD(void, write, (int addr, int value), (override));
-    //MOCK_METHOD(int, read, (int addr), (override));
+    MOCK_METHOD(int, Read, (int LBA), (override));
+    MOCK_METHOD(void, Write, (int LBA, int Data), (override));
 };
 
-class TestShellFixture : public testing::Test
-{
-protected:
-    void SetUp() override
-    {
+class TestShellFixture : public testing::Test {
+ protected:
+    void SetUp() override {
     }
-public:
+
+ public:
+     MockSsdDriver msd;
 };
 
-TEST_F(TestShellFixture, unmap_read)
-{
-    EXPECT_EQ(0, 0);
+TEST_F(TestShellFixture, unmap_read) {
+    EXPECT_CALL(msd, Read)
+        .WillRepeatedly(Return(0));
+
+    int data = msd.Read(0);
+    EXPECT_EQ(data, 0);
 }
 
-TEST_F(TestShellFixture, unmap_read_oor)
-{
-    EXPECT_EQ(0, 0);
+TEST_F(TestShellFixture, unmap_full_read) {
+    EXPECT_CALL(msd, Read)
+        .WillRepeatedly(Return(0));
+
+    for (int i = 0; i < 100; i++)
+    {
+        int data = msd.Read(i);
+        EXPECT_EQ(data, 0);
+    }
 }
