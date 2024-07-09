7d463560f7f3a429f586b908fc1750097fc37ff1
doyun.kim <gbyl2024@gmail.com>
2024-07-09 16:16:45 +0900

    fix access modifier

--- virtual_SSD/SSD.h
+++ virtual_SSD/SSD.h
@@ -7,16 +7,7 @@ class SSD {
  public:
   virtual void Write(const int& LBA, const std::string& data);
   virtual std::string Read(const int& LBA);
-  void ReadMemory();
-  void UpdateMemory(const int& LBA, const std::string& data);
-  void StoreMemory();
-  void CheckWriteCondition(const int& LBA, const std::string& data);
-  void CheckDataLength(const std::string& data);
-  void CheckLBARange(const int& LBA);
-  void CheckExistNandFile();
-  const std::string &ReturnReadData(const int &LBA);
-  void WriteResultFile(const int &LBA);
-
+  
  private:
   std::map<int, std::string> memory;
   std::string InitialLBAData{ "0x00000000" };
@@ -24,4 +15,14 @@ class SSD {
   std::string ReadFileName{ "result.txt" };
   const int MAX_LBA{ 99 };
   const int MIN_LBA{ 0 };
+
+  void ReadMemory();
+  void UpdateMemory(const int& LBA, const std::string& data);
+  void StoreMemory();
+  void CheckWriteCondition(const int& LBA, const std::string& data);
+  void CheckDataLength(const std::string& data);
+  void CheckLBARange(const int& LBA);
+  void CheckExistNandFile();
+  const std::string& ReturnReadData(const int& LBA);
+  void WriteResultFile(const int& LBA);
 };
