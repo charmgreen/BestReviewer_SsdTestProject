280da38cea157d85fd8ed7da671fc9d94d97525d
JJY <binaryfalse81@gmail.com>
2024-07-10 08:07:25 +0900

    [refactor] merge 하면서 test.cpp 누락된 내용 반영 test.cpp -> TestShellUnitTest.cpp 로 이름 변경
    [refactor] vitualSSD-Test 에서 test.cpp 제거
    
    [refactor] system call 의 virtual_SSD.exe path 를 Release 와 Debug build config 에 따라 다르게 처리함
    
    [refactor] merge 하면서 test.cpp 누락된 내용 반영
    test.cpp -> TestShellUnitTest.cpp 로 이름 변경
    
    [refactor] vitualSSD-Test 에서 test.cpp 제거
    
    [refactor] system call 의 virtual_SSD.exe path 를 Release 와 Debug build config 에 따라 다르게 처리함
    
    [feature] Test_Shell_App 에서 main() 함수 추가하여 명령어 실행 가능한 구조로 수정함
    
    [feature] Real Ssd Driver 에서 result.txt 에서 Read 해서 출력할 수 있도록 수정함
    
    [feature] Test_Shell_App.exe runlist.lst
    
    [refactor] virtual_SSD.exe -> SSD.exe 로 파일 명 수정

--- Test_Shell_App/Main_TestShell.cpp
+++ Test_Shell_App/Main_TestShell.cpp
@@ -1,4 +1,52 @@
 // Copyright [2024] <CRA/BestReviewer>
-int main() {
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "TestShell.h"
+#include "RealSsdDriver.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    TestShell TestShellApp;
+    TestShellApp.SetSsdDriver(new RealSsdDriver());
+
+    if (argc == 1)
+    {
+        while (true) {
+            string command;
+            getline(cin, command);
+            try {
+                TestShellApp.Run(command);
+            }
+            catch (ExitTestShell) {
+                break;
+            }
+        }
+    }
+    else if (argc == 2)
+    {
+        string inputArg = argv[1];
+        string ReadFileName{ inputArg };
+        ifstream resultFile(ReadFileName);
+        string command;
+
+        if (resultFile.is_open()) {
+            while (getline(resultFile, command)) {
+                cout << command << endl;
+                try {
+                    TestShellApp.Run(command);
+                }
+                catch (ExitTestShell) {
+                    break;
+                }
+            }
+            resultFile.close();
+        }
+        else {
+            cerr << "Open Error " + ReadFileName << endl;
+        }
+    }
+
     return 0;
 }
