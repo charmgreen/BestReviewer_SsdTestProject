33c32c6326a37612ea3208b698977901151073dc
doyun.kim <gbyl2024@gmail.com>
2024-07-09 14:07:47 +0900

    fix indent in SSD_Unittest.cpp

--- virtualSSD-Test/SSD_Unittest.cpp
+++ virtualSSD-Test/SSD_Unittest.cpp
@@ -26,32 +26,32 @@ class SSDFixture : public testing::Test {
 };
 
 TEST_F(SSDFixture, TestLBARangeExceptionWhenWrite) {
-  EXPECT_THROW(ssd.Write(100, "0x10000000"), LBARangeException);
+    EXPECT_THROW(ssd.Write(100, "0x10000000"), LBARangeException);
 }
 
 TEST_F(SSDFixture, TestDataRangeException) {
-  EXPECT_THROW(ssd.Write(99, "0x100000"), DataRangeException);
+    EXPECT_THROW(ssd.Write(99, "0x100000"), DataRangeException);
 }
 
 TEST_F(SSDFixture, TestWriteMemory) {
-  ssd.Write(0, "0x10000001");
-  EXPECT_EQ("0x10000001", getLSBData(0));
-  ssd.Write(3, "0x10000099");
-  EXPECT_EQ("0x10000099", getLSBData(3));
+    ssd.Write(0, "0x10000001");
+    EXPECT_EQ("0x10000001", getLSBData(0));
+    ssd.Write(3, "0x10000099");
+    EXPECT_EQ("0x10000099", getLSBData(3));
 }
 
 TEST_F(SSDFixture, TestReadMemory) {
-  EXPECT_THAT("0x10000001", ssd.Read(0));
+    EXPECT_THAT("0x10000001", ssd.Read(0));
 }
 
 TEST_F(SSDFixture, TestReadMemoryWhenEmpty) {
-  EXPECT_THAT("0x00000000", ssd.Read(1));
+    EXPECT_THAT("0x00000000", ssd.Read(1));
 }
 
 TEST_F(SSDFixture, TestMinusLBARangeExceptionWhenRead) {
-  EXPECT_THROW(ssd.Read(-10), LBARangeException);
+    EXPECT_THROW(ssd.Read(-10), LBARangeException);
 }
 
 TEST_F(SSDFixture, Test100LBARangeExceptionWhenRead) {
-  EXPECT_THROW(ssd.Read(100), LBARangeException);
+    EXPECT_THROW(ssd.Read(100), LBARangeException);
 }
