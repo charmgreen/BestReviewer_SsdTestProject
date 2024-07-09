17146306f51f15d507e95fe9f13ddc33004375f5
JJY <binaryfalse81@gmail.com>
2024-07-09 10:25:39 +0900

    [feature] Real Ssd Driver 에 System Call 을 Virtual_SSD.exe 로 변경함 (기존 Dummy Exe fill)

--- Test_Shell_App/RealSsdDriver.cpp
+++ Test_Shell_App/RealSsdDriver.cpp
@@ -4,12 +4,13 @@
 
 void RealSsdDriver::SystemCall(std::string cmdLine)
 {
-    std::string ssd_exe_path = "..\\Test_Shell_App\\SSD.exe";
-    ssd_exe_path += " ";
-    ssd_exe_path += cmdLine;
+//    std::string virtual_ssd_exe_path = "..\\Test_Shell_App\\SSD.exe";
+    std::string virtual_ssd_exe_path = "..\\x64\\Debug\\virtual_SSD.exe";
+    virtual_ssd_exe_path += " ";
+    virtual_ssd_exe_path += cmdLine;
 
     // b 프로젝트의 실행 파일을 시스템 호출로 실행
-    int result = system(ssd_exe_path.c_str());
+    int result = system(virtual_ssd_exe_path.c_str());
 
     if (result == 0) {
         // b.exe가 성공적으로 실행되었음
