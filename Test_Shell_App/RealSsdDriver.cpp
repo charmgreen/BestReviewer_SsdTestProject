a42b8a5c706dbd229d7f2afacb06043be4da65e0
JJY <binaryfalse81@gmail.com>
2024-07-10 10:48:59 +0900

    [feature] Real Ssd Driver 에서 result.txt 에서 Read 해서 출력할 수 있도록 수정함

--- Test_Shell_App/RealSsdDriver.cpp
+++ Test_Shell_App/RealSsdDriver.cpp
@@ -1,33 +1,49 @@
 // Copyright 2024, Samsung
 #include "RealSsdDriver.h"
 #include <iostream>
+#include <fstream>
 #include <string>
 
-std::string RealSsdDriver::Read(int LBA) {
-    std::string cmdLine = "R " + std::to_string(LBA);
+using namespace std;
+
+string RealSsdDriver::Read(int LBA) {
+    string cmdLine = "R " + to_string(LBA);
     SystemCall(cmdLine);
-    return cmdLine;  // temp
+    string ReadFileName{ "result.txt" };
+    ifstream resultFile(ReadFileName);
+    string line;
+
+    if (resultFile.is_open()) {
+        while (getline(resultFile, line)) {
+            cout << line << endl;
+        }
+        resultFile.close();
+    }
+    else {
+        cerr << "Open Error " + ReadFileName << endl;
+    }
+
+    return cmdLine;
 }
 
 void RealSsdDriver::Write(int LBA, std::string Data) {
-    std::string cmdLine = "W " + std::to_string(LBA) + " " + Data;
+    string cmdLine = "W " + to_string(LBA) + " " + Data;
     SystemCall(cmdLine);
 }
 
 void RealSsdDriver::SystemCall(std::string cmdLine) {
 #ifdef _DEBUG
-    std::string virtual_ssd_exe_path = "..\\x64\\Debug\\virtual_SSD.exe";
+    string virtual_ssd_exe_path = "..\\x64\\Debug\\virtual_SSD.exe";
 #else
-    std::string virtual_ssd_exe_path = "virtual_SSD.exe";
+    string virtual_ssd_exe_path = "virtual_SSD.exe";
 #endif
     virtual_ssd_exe_path += " ";
     virtual_ssd_exe_path += cmdLine;
 
     int result = system(virtual_ssd_exe_path.c_str());
 
-    if (result == 0) {
-        std::cout << "virtual_SSD.exe executed successfully.\n";
-    } else {
-        std::cerr << "Failed to execute virtual_SSD.exe. Error code: " << result << '\n';
+    if (result)
+    {
+        cerr << "Failed to execute virtual_SSD.exe. Error code: " << result << '\n';
     }
 }
