70dfcd78665f9404ad97a927e472efb55b951707
JJY <binaryfalse81@gmail.com>
2024-07-10 11:28:00 +0900

    [feature] Test_Shell_App.exe runlist.lst

--- Test_Shell_App/Main_TestShell.cpp
+++ Test_Shell_App/Main_TestShell.cpp
@@ -1,21 +1,55 @@
 // Copyright [2024] <CRA/BestReviewer>
 #include <iostream>
+#include <fstream>
 #include <string>
 #include "TestShell.h"
 #include "RealSsdDriver.h"
 
-int main() {
+using namespace std;
+
+int main(int argc, char* argv[]) {
     TestShell TestShellApp;
     TestShellApp.SetSsdDriver(new RealSsdDriver());
-    while (true) {
-        std::string command;
-        std::getline(std::cin, command);
-        try {
-            TestShellApp.Run(command);
-        }
-        catch (ExitTestShell) {
-            break;
+
+    if (argc == 1)
+    {
+        while (true) {
+            string command;
+            getline(cin, command);
+            try {
+                TestShellApp.Run(command);
+            }
+            catch (ExitTestShell) {
+                break;
+            }
         }
     }
+    else if (argc == 2)
+    {
+        string inputArg = argv[1];
+
+        cout << inputArg << endl;
+
+        string ReadFileName{ inputArg };
+        ifstream resultFile(ReadFileName);
+        string command;
+
+        if (resultFile.is_open()) {
+            while (getline(resultFile, command)) {
+                cout << command << endl;
+                try {
+                    TestShellApp.Run(command);
+                }
+                catch (ExitTestShell) {
+                    break;
+                }
+            }
+            resultFile.close();
+        }
+        else {
+            cerr << "Open Error " + ReadFileName << endl;
+        }
+    }
+
     return 0;
 }
